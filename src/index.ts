4e545d60cc07780208b5a06f0ab77eb3744e6b45
JiveOff <antoine@jiveoff.fr>
2023-02-14 10:05:11 +0100

    🏠 Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+
+import { resolvers } from "@generated/type-graphql";
+import { buildSchema } from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const initServer = async () => {
+  const schema = await buildSchema({
+    resolvers,
+    validate: false,
+  });
+
+  const server = new ApolloServer({ schema, context: () => ({ prisma }) });
+
+  server.listen({ port: 3000 }).then(r => {
+    console.log(`Server ready at ${r.url}`);
+  });
+}
+
+initServer()
